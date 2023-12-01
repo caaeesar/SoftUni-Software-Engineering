f0eef8584e2a842431683a4947616740d3ff1a8b
Melissa Rudeva <105941093+caaeesar@users.noreply.github.com>
2023-12-01 20:35:47 +0200

    upload

--- Java Web/Spring-Advanced/CookingAdventure/src/main/java/bg/softuni/cookingadventure/exception/UserDisabledException.java
+++ Java Web/Spring-Advanced/CookingAdventure/src/main/java/bg/softuni/cookingadventure/exception/UserDisabledException.java
@@ -1,7 +1,10 @@
 package bg.softuni.cookingadventure.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(code = HttpStatus.FORBIDDEN)
 public class UserDisabledException extends UsernameNotFoundException {
 
     public UserDisabledException(String username) {
